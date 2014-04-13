6f9e90d259496228bbe27f03c7ea7c8a00dee27f
bob walker <bob@randomness.org.uk>
2014-04-13 19:04:37 +0100

    Script to create a draft post

---
+++ bin/create-post.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# script to generate a draft post
+TITLE=$@
+POST=${TITLE// /-}
+if [ -e _drafts/${POST}.md ]
+then
+  echo "_drafts/${POST}.md already exists"
+else
+  echo "creating _drafts/${POST}.md"
+  echo "---" >> _drafts/${POST}.md
+  echo "layout: post" >> _drafts/${POST}.md
+  echo "title: $TITLE" >> _drafts/${POST}.md
+  echo "---" >> _drafts/${POST}.md
+fi
